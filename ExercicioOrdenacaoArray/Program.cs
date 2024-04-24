fdf64ed345e1d80ad0456392197a2a4098772604
gabsoares <soaresgabriel68@gmail.com>
2024-04-23 23:13:51 -0300

    Add project files.

---
+++ ExercicioOrdenacaoArray/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] vetor = new int[5] { 58, 8, 31, 1000, 8 };
+int[] vetor2 = vetor;
+int aux;
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = (i + 1); j < 5; j++)
+    {
+        if (vetor2[i] > vetor2[j])
+        {
+            aux = vetor2[i];
+            vetor2[i] = vetor2[j];
+            vetor2[j] = aux;
+        }
+    }
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(vetor2[i] + " ");
+}
