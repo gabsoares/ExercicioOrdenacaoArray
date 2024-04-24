821e5c6d3ecb5fb0b25524fa3d0c3ced84a132fc
gabsoares <soaresgabriel68@gmail.com>
2024-04-24 00:42:51 -0300

    Feature - implementação para ordem decrescente

--- ExercicioOrdenacaoArray/Program.cs
+++ ExercicioOrdenacaoArray/Program.cs
@@ -1,8 +1,9 @@
-﻿int[] vetor = new int[5] { 58, 8, 31, 58, 8 };
+﻿int[] vetor = new int[5] { 100, 300, 15000, 8, 1 };
 int[] vetor2 = vetor;
 int[] vetor3 = vetor2;
-int aux, aux2;
+int aux;
 
+//Ordem crescente
 for (int i = 0; i < 5; i++)
 {
     for (int j = (i + 1); j < 5; j++)
@@ -16,16 +17,19 @@ for (int i = 0; i < 5; i++)
     }
 }
 
-for (int i = 0; i < 5; i++)
-{
-    for (int j = (i + 1); j < 5; j++)
-    {
-        if (vetor2[i] > vetor2[j])
-        {
-
-        }
-    }
-}
+//Ordem decrescente
+//for (int i = 0; i < 5; i++)
+//{
+//    for (int j = (i + 1); j < 5; j++)
+//    {
+//        if (vetor3[i] < vetor3[j])
+//        {
+//            aux = vetor2[i];
+//            vetor2[i] = vetor2[j];
+//            vetor2[j] = aux;
+//        }
+//    }
+//}
 
 for (int i = 0; i < 5; i++)
 {
