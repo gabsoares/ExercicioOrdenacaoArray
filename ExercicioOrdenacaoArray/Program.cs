e186f739af1153e3e4e472f838736ffba12704b1
gabsoares <soaresgabriel68@gmail.com>
2024-04-23 23:15:50 -0300

    Feature - tentando retirar número duplicado

--- ExercicioOrdenacaoArray/Program.cs
+++ ExercicioOrdenacaoArray/Program.cs
@@ -1,6 +1,7 @@
-﻿int[] vetor = new int[5] { 58, 8, 31, 1000, 8 };
+﻿int[] vetor = new int[5] { 58, 8, 31, 58, 8 };
 int[] vetor2 = vetor;
-int aux;
+int[] vetor3 = vetor2;
+int aux, aux2;
 
 for (int i = 0; i < 5; i++)
 {
@@ -17,5 +18,21 @@ for (int i = 0; i < 5; i++)
 
 for (int i = 0; i < 5; i++)
 {
+    for (int j = (i + 1); j < 5; j++)
+    {
+        if (vetor2[i] > vetor2[j])
+        {
+
+        }
+    }
+}
+
+for (int i = 0; i < 5; i++)
+{
     Console.Write(vetor2[i] + " ");
+}
+Console.WriteLine();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write(vetor3[i] + " ");
 }
